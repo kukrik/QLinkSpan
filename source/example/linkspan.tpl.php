78e0d3d384bf892479991ffb12924c29a3cf4a35
kukrik <tiit.papp@gmail.com>
2013-07-30 16:38:11 +0300

    Uptated

---
+++ source/example/linkspan.tpl.php
@@ -0,0 +1,239 @@
+<?php require(__DOCROOT__ . __EXAMPLES__ . '/includes/header.inc.php'); ?>
+	<?php $this->RenderBegin(); ?>
+    
+    
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:300' rel='stylesheet' type='text/css'>
+
+	<div id="instructions">
+		<h1 class="instruction_title">QLinkSpan: Is it possible to change the link to exciting</h1>
+        
+        <p>The standard QLinkButton and QButton controls don't allow the user to change links with desired appearances. Using QLinkSpan is easy and offers more opportunities. CSS is great friend. There is no need to code it in a complicated way to get these buttons. You just have to change the files of the styles, if you want. Entypo icons offer also more opportunities. And this is how you can use QLinkSpan according to your needs:<br />
+        <br />
+      - PlaceSpan = QPlaceSpan::Left or PlaceSpan = QPlaceSpan::Right or without them (the same principle as PlaceSpan = QPlaceSpan::Left),<br />
+      - SpanText ="xxxxxxxx",<br />
+      - SecondSpanText ="xxxxxxxx",<br />
+      - SubText ="xxxxxxxx".<br />
+        <br />
+        You will understand it more by exploring the source of QLinkSpan.class.</p>
+<hr />   
+	<h6>Colors</h6>
+    
+    	<p>The buttons can benefit from the template color sets.</p><p>&nbsp;</p>
+					<h6>Gradients</h6>
+					<p class="button-height">
+						<a href="#" class="button black-gradient">Black</a>
+						<a href="#" class="button anthracite-gradient">Anthracite</a>
+						<a href="#" class="button grey-gradient">Grey</a>
+						<a href="#" class="button white-gradient">White</a>
+						<a href="#" class="button red-gradient">Red</a>
+						<a href="#" class="button orange-gradient">Orange</a>
+						<a href="#" class="button green-gradient">Green</a>
+						<a href="#" class="button blue-gradient">Blue</a>
+					</p>
+
+					<h6>Glossy gradients</h6>
+					<p class="button-height">
+						<a href="#" class="button black-gradient glossy">Black</a>
+						<a href="#" class="button anthracite-gradient glossy">Anthracite</a>
+						<a href="#" class="button grey-gradient glossy">Grey</a>
+						<a href="#" class="button white-gradient glossy">White</a>
+						<a href="#" class="button red-gradient glossy">Red</a>
+						<a href="#" class="button orange-gradient glossy">Orange</a>
+						<a href="#" class="button green-gradient glossy">Green</a>
+						<a href="#" class="button blue-gradient glossy">Blue</a>
+					</p>
+
+					<h6>Disabled</h6>
+					<p class="button-height">
+						<a href="#" class="button black-gradient disabled">Black</a>
+						<a href="#" class="button anthracite-gradient disabled">Anthracite</a>
+						<a href="#" class="button grey-gradient disabled">Grey</a>
+						<a href="#" class="button white-gradient disabled">White</a>
+						<a href="#" class="button red-gradient disabled">Red</a>
+						<a href="#" class="button orange-gradient disabled">Orange</a>
+						<a href="#" class="button green-gradient disabled">Green</a>
+						<a href="#" class="button blue-gradient disabled">Blue</a>
+					</p>
+
+					<h6>Custom active color (click the buttons)</h6>
+					<p class="button-height">
+						<a href="#" class="button black-active">Black</a>
+						<a href="#" class="button anthracite-active">Anthracite</a>
+						<a href="#" class="button grey-active">Grey</a>
+						<a href="#" class="button white-active">White</a>
+						<a href="#" class="button red-active">Red</a>
+						<a href="#" class="button orange-active">Orange</a>
+						<a href="#" class="button green-active">Green</a>
+						<a href="#" class="button blue-active">Blue</a>
+					</p>
+                    
+	<hr />
+			<h6>Icons</h6>
+			<p>The template includes a complete icon set from the awesome icon font <a href="http://www.entypo.com/"><strong>Entypo</strong></a>, with <strong>122</strong> available symbols:</p><p>&nbsp;</p>
+
+			<ul class="blocks-list square-46">
+				<li class="icon-phone icon-size2" title="icon-phone"></li>
+				<li class="icon-mobile icon-size2" title="icon-mobile"></li>
+				<li class="icon-tag icon-size2" title="icon-tag"></li>
+				<li class="icon-directions icon-size2" title="icon-directions"></li>
+				<li class="icon-mail icon-size2" title="icon-mail"></li>
+				<li class="icon-pencil icon-size2" title="icon-pencil"></li>
+				<li class="icon-paperclip icon-size2" title="icon-paperclip"></li>
+				<li class="icon-reply icon-size2" title="icon-reply"></li>
+				<li class="icon-replay-all icon-size2" title="icon-replay-all"></li>
+				<li class="icon-forward icon-size2" title="icon-forward"></li>
+				<li class="icon-user icon-size2" title="icon-user"></li>
+				<li class="icon-users icon-size2" title="icon-users"></li>
+				<li class="icon-add-user icon-size2" title="icon-add-user"></li>
+				<li class="icon-card icon-size2" title="icon-card"></li>
+				<li class="icon-extract icon-size2" title="icon-extract"></li>
+				<li class="icon-marker icon-size2" title="icon-marker"></li>
+				<li class="icon-map icon-size2" title="icon-map"></li>
+				<li class="icon-compass icon-size2" title="icon-compass"></li>
+				<li class="icon-arrow icon-size2" title="icon-arrow"></li>
+				<li class="icon-target icon-size2" title="icon-target"></li>
+				<li class="icon-path icon-size2" title="icon-path"></li>
+				<li class="icon-heart icon-size2" title="icon-heart"></li>
+				<li class="icon-star icon-size2" title="icon-star"></li>
+				<li class="icon-like icon-size2" title="icon-like"></li>
+				<li class="icon-chat icon-size2" title="icon-chat"></li>
+				<li class="icon-speech icon-size2" title="icon-speech"></li>
+				<li class="icon-quote icon-size2" title="icon-quote"></li>
+				<li class="icon-printer icon-size2" title="icon-printer"></li>
+				<li class="icon-bell icon-size2" title="icon-bell"></li>
+				<li class="icon-link icon-size2" title="icon-link"></li>
+				<li class="icon-flag icon-size2" title="icon-flag"></li>
+				<li class="icon-gear icon-size2" title="icon-gear"></li>
+				<li class="icon-flashlight icon-size2" title="icon-flashlight"></li>
+				<li class="icon-cup icon-size2" title="icon-cup"></li>
+				<li class="icon-price-tag icon-size2" title="icon-price-tag"></li>
+				<li class="icon-camera icon-size2" title="icon-camera"></li>
+				<li class="icon-moon icon-size2" title="icon-moon"></li>
+				<li class="icon-palette icon-size2" title="icon-palette"></li>
+				<li class="icon-leaf icon-size2" title="icon-leaf"></li>
+				<li class="icon-music-note icon-size2" title="icon-music-note"></li>
+				<li class="icon-bag icon-size2" title="icon-bag"></li>
+				<li class="icon-plane icon-size2" title="icon-plane"></li>
+				<li class="icon-buoy icon-size2" title="icon-buoy"></li>
+				<li class="icon-rain icon-size2" title="icon-rain"></li>
+				<li class="icon-eye icon-size2" title="icon-eye"></li>
+				<li class="icon-clock icon-size2" title="icon-clock"></li>
+				<li class="icon-mic icon-size2" title="icon-mic"></li>
+				<li class="icon-calendar icon-size2" title="icon-calendar"></li>
+				<li class="icon-lightning icon-size2" title="icon-lightning"></li>
+				<li class="icon-hourglass icon-size2" title="icon-hourglass"></li>
+				<li class="icon-rss icon-size2" title="icon-rss"></li>
+				<li class="icon-wifi icon-size2" title="icon-wifi"></li>
+				<li class="icon-lock icon-size2" title="icon-lock"></li>
+				<li class="icon-unlock icon-size2" title="icon-unlock"></li>
+				<li class="icon-tick icon-size2" title="icon-tick"></li>
+				<li class="icon-cross icon-size2" title="icon-cross"></li>
+				<li class="icon-minus-round icon-size2" title="icon-minus-round"></li>
+				<li class="icon-plus-round icon-size2" title="icon-plus-round"></li>
+				<li class="icon-cross-round icon-size2" title="icon-cross-round"></li>
+				<li class="icon-minus icon-size2" title="icon-minus"></li>
+				<li class="icon-plus icon-size2" title="icon-plus"></li>
+				<li class="icon-forbidden icon-size2" title="icon-forbidden"></li>
+				<li class="icon-info icon-size2" title="icon-info"></li>
+				<li class="icon-info-round icon-size2" title="icon-info-round"></li>
+				<li class="icon-question icon-size2" title="icon-question"></li>
+				<li class="icon-question-round icon-size2" title="icon-question-round"></li>
+				<li class="icon-warning icon-size2" title="icon-warning"></li>
+				<li class="icon-redo icon-size2" title="icon-redo"></li>
+				<li class="icon-undo icon-size2" title="icon-undo"></li>
+				<li class="icon-swap icon-size2" title="icon-swap"></li>
+				<li class="icon-revert icon-size2" title="icon-revert"></li>
+				<li class="icon-refresh icon-size2" title="icon-refresh"></li>
+				<li class="icon-list icon-size2" title="icon-list"></li>
+				<li class="icon-list-add icon-size2" title="icon-list-add"></li>
+				<li class="icon-thumbs icon-size2" title="icon-thumbs"></li>
+				<li class="icon-page-list icon-size2" title="icon-page-list"></li>
+				<li class="icon-page icon-size2" title="icon-page"></li>
+				<li class="icon-pages icon-size2" title="icon-pages"></li>
+				<li class="icon-marker icon-size2" title="icon-marker"></li>
+				<li class="icon-pictures icon-size2" title="icon-pictures"></li>
+				<li class="icon-movie icon-size2" title="icon-movie"></li>
+				<li class="icon-music icon-size2" title="icon-music"></li>
+				<li class="icon-folder icon-size2" title="icon-folder"></li>
+				<li class="icon-drawer icon-size2" title="icon-drawer"></li>
+				<li class="icon-trash icon-size2" title="icon-trash"></li>
+				<li class="icon-outbox icon-size2" title="icon-outbox"></li>
+				<li class="icon-inbox icon-size2" title="icon-inbox"></li>
+				<li class="icon-download icon-size2" title="icon-download"></li>
+				<li class="icon-cloud icon-size2" title="icon-cloud"></li>
+				<li class="icon-cloud-upload icon-size2" title="icon-cloud-upload"></li>
+				<li class="icon-play icon-size2" title="icon-play"></li>
+				<li class="icon-pause icon-size2" title="icon-pause"></li>
+				<li class="icon-record icon-size2" title="icon-record"></li>
+				<li class="icon-forward icon-size2" title="icon-forward"></li>
+				<li class="icon-backward icon-size2" title="icon-backward"></li>
+				<li class="icon-previous icon-size2" title="icon-previous"></li>
+				<li class="icon-next icon-size2" title="icon-next"></li>
+				<li class="icon-expand icon-size2" title="icon-expand"></li>
+				<li class="icon-reduce icon-size2" title="icon-reduce"></li>
+				<li class="icon-volume icon-size2" title="icon-volume"></li>
+				<li class="icon-loud icon-size2" title="icon-loud"></li>
+				<li class="icon-mute icon-size2" title="icon-mute"></li>
+				<li class="icon-left-fat icon-size2" title="icon-left-fat"></li>
+				<li class="icon-down-fat icon-size2" title="icon-down-fat"></li>
+				<li class="icon-up-fat icon-size2" title="icon-up-fat"></li>
+				<li class="icon-right-fat icon-size2" title="icon-right-fat"></li>
+				<li class="icon-left icon-size2" title="icon-left"></li>
+				<li class="icon-down icon-size2" title="icon-down"></li>
+				<li class="icon-up icon-size2" title="icon-up"></li>
+				<li class="icon-right icon-size2" title="icon-right"></li>
+				<li class="icon-left-round icon-size2" title="icon-left-round"></li>
+				<li class="icon-down-round icon-size2" title="icon-down-round"></li>
+				<li class="icon-up-round icon-size2" title="icon-up-round"></li>
+				<li class="icon-right-round icon-size2" title="icon-right-round"></li>
+				<li class="icon-home icon-size2" title="icon-home"></li>
+				<li class="icon-ribbon icon-size2" title="icon-ribbon"></li>
+				<li class="icon-read icon-size2" title="icon-read"></li>
+				<li class="icon-new-tab icon-size2" title="icon-new-tab"></li>
+				<li class="icon-search icon-size2" title="icon-search"></li>
+				<li class="icon-ellipsis icon-size2" title="icon-ellipsis"></li>
+				<li class="icon-bullet-list icon-size2" title="icon-bullet-list"></li>
+				<li class="icon-creative-commons icon-size2" title="icon-creative-commons"></li>
+			</ul>	
+	</div>
+    
+    <div id="demoZone">
+  
+    
+    <h3>The different opportunities and appearances of the buttons</h3>
+    <p>You can see more opportunities by clicking on the buttons.</p><br />
+    
+    <p class="button-height">
+	<?php $this->btnSave->Render(); ?>
+	<?php $this->btnCancel->Render(); ?>
+	<?php $this->btnDelete->Render(); ?>
+	<?php $this->btnDelete0->Render(); ?>
+    </p>
+    
+     <p class="button-height">
+	 <?php $this->btnCreate->Render(); ?>
+	 <?php $this->btnDuplicate->Render(); ?>
+	 <?php $this->btnMessage->Render(); ?>
+	 <?php $this->btnIcons->Render(); ?>
+    </p>
+    
+    <p>Buttons can be grouped.</p><br />
+    <p class="button-height">
+	<span class="button-group">
+	<?php $this->btnCreate_Group->Render(); ?>
+	<?php $this->btnSave_Group->Render(); ?>
+	<?php $this->btnDuplicate_Group->Render(); ?>
+	<?php $this->btnDelete_Group->Render(); ?>
+     </span>
+     </p>
+         
+	<p class="button-height">
+    <span class="button-group">
+	<?php $this->btnList_Two->Render(); ?>
+	<?php $this->btnIcons_Two->Render(); ?>
+	</span>
+    </p>
+        </div>
+
+	<?php $this->RenderEnd(); ?>
+<?php require(__DOCROOT__ . __EXAMPLES__ . '/includes/footer.inc.php'); ?>
